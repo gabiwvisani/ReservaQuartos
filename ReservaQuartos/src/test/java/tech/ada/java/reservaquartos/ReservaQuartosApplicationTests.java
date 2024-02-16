12bbb41221f61a36477a823e8aa14734fc53f97a
Gabriela Whitaker Visani <38706373+gabiwvisani@users.noreply.github.com>
2024-02-16 16:44:43 -0300

    Add files via upload

---
+++ ReservaQuartos/src/test/java/tech/ada/java/reservaquartos/ReservaQuartosApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.ada.java.reservaquartos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservaQuartosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

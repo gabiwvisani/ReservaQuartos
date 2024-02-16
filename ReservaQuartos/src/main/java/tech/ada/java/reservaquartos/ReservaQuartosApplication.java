707333c2fd46aef76cd5a9d96e9411cb9510b09a
Gabriela Whitaker Visani <38706373+gabiwvisani@users.noreply.github.com>
2024-02-16 16:36:50 -0300

    Add files via upload

---
+++ ReservaQuartos/src/main/java/tech/ada/java/reservaquartos/ReservaQuartosApplication.java
@@ -0,0 +1,13 @@
+package tech.ada.java.reservaquartos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservaQuartosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReservaQuartosApplication.class, args);
+    }
+
+}

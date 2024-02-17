f5424d927b3872c22943336dd62afac9edac1244
Gabriela Visani <gabriela.visani@aztronic.com.br>
2024-02-17 16:01:23 -0300

    criado os repositorios

--- ReservaQuartos/src/main/java/tech/ada/java/reservaquartos/domain/ReservaRepository.java
+++ ReservaQuartos/src/main/java/tech/ada/java/reservaquartos/domain/ReservaRepository.java
@@ -1,8 +1,9 @@
 package tech.ada.java.reservaquartos.domain;
 
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
+public interface ReservaRepository extends JpaRepository<Reserva, Integer> {
 }

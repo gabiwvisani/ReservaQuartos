6950a043b60f2a3e839e4f28939addace2f1f563
Gabriela Visani <gabriela.visani@aztronic.com.br>
2024-02-17 10:59:11 -0300

    atributo data reserva

--- ReservaQuartos/src/main/java/tech/ada/java/reservaquartos/domain/Reserva.java
+++ ReservaQuartos/src/main/java/tech/ada/java/reservaquartos/domain/Reserva.java
@@ -1,5 +1,8 @@
 package tech.ada.java.reservaquartos.domain;
 
+import java.time.LocalDateTime;
+
 public class Reserva {
     private Integer identificadorReserva;
+    private LocalDateTime dataRealizacaoReserva;
 }

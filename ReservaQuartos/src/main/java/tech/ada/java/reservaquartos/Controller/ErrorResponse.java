a18b3835f43c50a5caac28ae165004d2171a9f1f
Isabel Kodama <kodama.isabel@gmail.com>
2024-02-18 16:22:10 -0300

    getClientePorId

---
+++ ReservaQuartos/src/main/java/tech/ada/java/reservaquartos/Controller/ErrorResponse.java
@@ -0,0 +1,14 @@
+package tech.ada.java.reservaquartos.Controller;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ErrorResponse {
+    private String message;
+}
--- ReservaQuartos/src/main/java/tech/ada/java/reservaquartos/Repository/ClienteRepository.java
+++ ReservaQuartos/src/main/java/tech/ada/java/reservaquartos/Repository/ClienteRepository.java
@@ -4,6 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import tech.ada.java.reservaquartos.domain.Entidades.Cliente;
 
+import java.util.Optional;
+
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
+    Optional<Cliente> findByCpf(String cpf);
 }
